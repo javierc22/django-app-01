91d2dc743ca09e70e133ef32e0fda975fded9eb4
javierc22 <javier.ec.22@gmail.com>
2020-09-18 19:33:10 -0300

    Agregar migraciones para panel administrador

--- core/erp/models.py
+++ core/erp/models.py
@@ -1,57 +1,82 @@
 from django.db import models
 from datetime import datetime
 
-class Type(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
+from core.erp.choices import gender_choices
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        ordering = ['id']
-        db_table = 'tipo'
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Nombre')
 
     def __str__(self):
-        return self.name
+        return "Id: {} / Name: {}".format(self.id, self.name)
 
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         ordering = ['id']
 
-class Employee(models.Model):
-    # Si un "Tipo" se elimina, todos los "Empleados" que dependen de ese "Tipo" quedan con el campo en NULL
-    # type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
-    # No se puede eliminar un "Tipo" si algún "Empleado" depende de ese "Tipo"
-    # type = models.ForeignKey(Type, on_delete=models.PROTECT)
 
-    # Si un "Tipo" se elimina, todos los "Empleados" que dependen de ese "Tipo" se eliminan
-    type = models.ForeignKey(Type, on_delete=models.CASCADE)
-    categ = models.ManyToManyField(Category)
+class Product(models.Model):
+    # Si una "Categoria" se elimina, todos los "Productos" que dependen de esa "Categoría" se eliminan
+    cate = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+
+class Client(models.Model):
     names = models.CharField(max_length=150, verbose_name='Nombres')
+    surnames = models.CharField(max_length=150, verbose_name='Apellidos')
     dni = models.CharField(max_length=10, unique=True, verbose_name='Dni')
-    date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de registro')
-    date_creation = models.DateTimeField(auto_now=True)
-    date_updated = models.DateTimeField(auto_now_add=True)
-    age = models.PositiveIntegerField(default=0)
-    salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
-    state = models.BooleanField(default=True)
-    avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', null=True, blank=True)
-    cvitae = models.FileField(upload_to='cvitae/%Y/%m/%d', null=True, blank=True)
+    birthday = models.DateField(default=datetime.now, verbose_name='Fecha de nacimiento')
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    sexo = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
 
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
-        # orden ascedente
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
         ordering = ['id']
-        # ordering = ['-id'] => orden descendente
+
+
+class Sale(models.Model):
+    cli = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.now)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    iva = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.cli.names
+
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        ordering = ['id']
+
+
+class DetSale(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    prod = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    cant = models.IntegerField(default=0)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.prod.name
+
+    class Meta:
+        verbose_name = 'Detalle de Venta'
+        verbose_name_plural = 'Detalle de Ventas'
+        ordering = ['id']
 
--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
+from config.wsgi import *
+from core.erp.models import Type
 
-# Create your tests here.
+# Listar
+
