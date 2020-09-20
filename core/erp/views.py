f866351ff5a3c13f0faf11c1e01243d5976d1df2
javierc22 <javier.ec.22@gmail.com>
2020-09-20 15:40:58 -0300

    Templates Parte 2

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from core.erp.models import Category
 
 
 def myfirstview(request):
     data = {
-        'name': 'Javier'
+        'name': 'Javier',
+        'categories': Category.objects.all()
     }
 
     return render(request, 'index.html', data)
